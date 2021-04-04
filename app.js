3974c6b3f7b69bdbf7ac3a1703aa528d93d85165
Cameron <yeec327@gmail.com>
2021-04-03 17:19:22 -0700

    Added in a request node module

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
 
+// this creates the express app so that you can use express
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+
+
+
+app.listen(3000, function() {
+    console.log("Server running on port 3000");
+});
