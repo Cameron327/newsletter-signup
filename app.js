30e0f712cf04d14266c7952790b9860d27a244a6
Cameron <yeec327@gmail.com>
2021-04-03 18:32:16 -0700

    Implemented POST request for the information

--- app.js
+++ app.js
@@ -5,12 +5,21 @@ const request = require("request");
 // this creates the express app so that you can use express
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.use(express.static("public"));
-
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + "/signup.html");
+});
+
+app.post('/hoopla', function (req, res) {
+    var firstName = req.body.fName;
+    var lastName = req.body.lName;
+    var email = req.body.email;
+
+    console.log(firstName);
+    console.log(lastName);
+    console.log(email);
+
 });
 
 
