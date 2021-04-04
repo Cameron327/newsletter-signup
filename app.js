c1c904f4ae68735400380f0ccde304ede1396bd1
Cameron <yeec327@gmail.com>
2021-04-03 18:24:02 -0700

    Utilized the express functon express.static to incorporate our static files

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const request = require("request");
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(express.static("public"));
 
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
 
 
 
